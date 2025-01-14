feeb54cc91d6596e4b4a460dd5316144e81b61e7
omar elhadedy <elhadede36@gmail.com>
2025-01-14 16:08:33 +0200

    create data layer in home and create book model with json to dart extension

--- lib/Features/home/domain/use_cases/fetch_best_seller_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_best_seller_books_use_case.dart
@@ -4,15 +4,13 @@ import 'package:bookly/core/Errors/failure.dart';
 import 'package:bookly/core/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>,
-    NoParam> //no param could be void but this looks more clean
-{
+class FetchBestSellerBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
 
-  FetchFeaturedBooksUseCase({required this.homeRepo});
+  FetchBestSellerBooksUseCase({required this.homeRepo});
 
   @override
   Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
-    return await homeRepo.fetchFeaturedBooks();
+    return await homeRepo.fetchBestSellerBooks();
   }
 }
--- lib/core/use_cases/use_case.dart
+++ lib/core/use_cases/use_case.dart
@@ -1,10 +1,8 @@
-
 import 'package:bookly/core/Errors/failure.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class UseCase<Type,Param>
-{
-  Future<Either<Failure,Type>> call([Param param]);
+abstract class UseCase<Type, Param> {
+  Future<Either<Failure, Type>> call([Param param]);
 }
 
-class NoParam{}
+class NoParam {}
